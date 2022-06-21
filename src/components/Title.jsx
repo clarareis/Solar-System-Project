81154282c333c6c848b63e3735df038b5c315b80
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-21 16:56:36 -0300

    Colocando uma prop na tag h2 no Componente Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{headline}</h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
+
+export default Title;
