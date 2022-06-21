7512d1e1b645635b50f2db36a5f6240a088389ca
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-21 18:28:38 -0300

    Criando um componente PlanetCard e renderizando uma imagem e seu texto alternativo com props.

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    const texto = `Planeta ${planetName}`;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ texto } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
