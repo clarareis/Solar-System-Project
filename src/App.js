eb25c77c181337159c67ad361934ac987f15e180
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-21 15:04:13 -0300

    Incluindo um componente chamado Header e renderizando ele no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (<p><Header /></p>);
   }
 }
 
