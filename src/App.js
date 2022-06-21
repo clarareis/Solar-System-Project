b58e53d2d3426acbc27e27d09ed873a4e75b77c8
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-21 15:19:57 -0300

    Criando o atributo SolarSystem com um atributo e incluindo no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p><Header /></p>);
+    return (
+      <div>
+        <h1><Header /></h1>
+        <p><SolarSystem /></p>
+      </div>
+    );
   }
 }
 
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">Teste </div>
+    );
+  }
+}
+
+export default SolarSystem;
