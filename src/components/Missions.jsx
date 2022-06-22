529188e10e61a45fc246354c484f5ab917309bab
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-22 18:28:47 -0300

    Renderizando o componente MissionCard no componente Missions.

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 import Title from './Title';
 
 class Missions extends React.Component {
@@ -6,6 +8,16 @@ class Missions extends React.Component {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission) => (
+          <MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />
+        ))}
+
       </div>
     );
   }
